74c269a9a18ba9b866f27452c1502a80da387426
Shaban <sh3ban.kamel@gmail.com>
2022-11-21 01:40:16 +0200

    Fix FakeDataSource issues

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,31 +2,24 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
-import net.bytebuddy.implementation.bytecode.Throw
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(var remindersList: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
+class FakeDataSource(var remindersList: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
     private var shouldReturnError = false
-
 
     fun setReturnError(value: Boolean) {
         shouldReturnError = value
     }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        if (shouldReturnError) {
-        return Result.Error(
-            "Error getting reminders"
-        )
-    }
-        remindersList?.let { return Result.Success(it) }
-        return Result.Error("Reminders not found")
+        if (shouldReturnError) return Result.Error("Error getting reminders")
+        val list = remindersList ?: return Result.Error("Reminders not found")
+        return Result.Success(list)
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-       remindersList?.add(reminder)
+        remindersList?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
@@ -34,17 +27,17 @@ class FakeDataSource(var remindersList: MutableList<ReminderDTO>? = mutableListO
             reminderDTO.id == id
         }
 
-       return when {
+        return when {
             shouldReturnError -> {
                 Result.Error("Reminder not found!")
             }
 
-           reminder != null -> {
-               Result.Success(reminder)
-           }
-           else -> {
-               Result.Error("Reminder not found!")
-           }
+            reminder != null -> {
+                Result.Success(reminder)
+            }
+            else -> {
+                Result.Error("Reminder not found!")
+            }
         }
     }
 
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -75,7 +75,4 @@ class SaveReminderViewModelTest {
         assertThat(viewModel.showLoading.getOrAwaitValue()).isFalse()
     }
 
-
-
-
 }
